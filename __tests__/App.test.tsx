67cc123a3f0b88013f58d0239cbff50132b73485
kaluza97 <piotrkaluzny@MacBook-Pro-Piotr.local>
2024-08-13 12:59:50 +0200

    jest and react testing configuration

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -3,15 +3,13 @@
  */
 
 import 'react-native';
-import React from 'react';
 import App from '../App';
+import React from 'react';
+import { render, screen } from '@testing-library/react-native';
 
-// Note: import explicitly to use the types shipped with jest.
-import {it} from '@jest/globals';
 
-// Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
-
-it('renders correctly', () => {
-  renderer.create(<App />);
+describe('truth', () => {
+  it('is true', () => {
+    expect(true).toEqual(true);
+  });
 });
--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,11 @@
 module.exports = {
   preset: 'react-native',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  transformIgnorePatterns: [
+    'node_modules/(?!(jest-)?react-native|@react-native|@react-navigation)'
+  ],
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|webp|svg)$': '<rootDir>/__mocks__/fileMock.js'
+  },
+  setupFilesAfterEnv: ['./jest-setup-after-env.ts'],
 };
